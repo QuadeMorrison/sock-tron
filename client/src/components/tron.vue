8358781c046ea350381d83fd8a9fea287a573c42
Alan Morgan <alanxoc3@gmail.com>
2018-03-31 12:46:06 -0600

    Better ending screen and text head.

--- client/src/components/tron.vue
+++ client/src/components/tron.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 div
   canvas#game_window(:width=`this.win_w + "px"`
-    :height=`this.win_h + "px"`
-      ref="game_window")
+	 :height=`this.win_h + "px"`
+		ref="game_window")
 </template>
 
 <script>
@@ -11,291 +11,286 @@ import keys from "../keys.js"
 export default {
   name: 'tron',
   data () {
-    return {
-      ctx: null,
-      canv_players: [],
-      pl_num: 0,
-      grid_dim: 0,
-      win_w: 0,  win_h: 0,
-      grid: 10,  pl_dim: 8,
-      win_list: null, FPS: 30,
-      count_down: 0, searching: false,
-      recv_delta: 0, last_time: (new Date()).getTime(),
-      delta_inc: 0, frame_delta: 0,
-      start_draw_players: false
-    }
+	 return {
+		ctx: null,
+		canv_players: [],
+		pl_num: 0,
+		grid_dim: 0,
+		win_w: 0,  win_h: 0,
+		grid: 10,  pl_dim: 8,
+		win_list: null, FPS: 30,
+		count_down: 0, searching: false,
+		recv_delta: 0, last_time: (new Date()).getTime(),
+		delta_inc: 0, frame_delta: 0,
+		start_draw_players: false
+	 }
   },
   created() {
-    this.draw_interval_id = window.setInterval(this.draw, 1000 / this.FPS)
+	 this.draw_interval_id = window.setInterval(this.draw, 1000 / this.FPS)
   },
   sockets: {
-    connect() {
-      console.log("Connected to server")
-      this.$socket.emit('enter_room')
-    },
-    game_over(players) {
-      this.win_list = players;
-      this.delta_inc = 0;
-      this.frame_delta = 0;
-    },
-    game_starts_in(data) {
-      this.start_draw_players = false
-      this.canv_players = []
-      this.count_down = data.count_down;
-      this.scale_window(data.settings)
-      this.init_players(data.players)
-      this.start_draw_players = true
-    },
-    init_settings(settings) {
-      // consistent naming
-      this.scale_window(settings)
-      this.ctx = this.$refs.game_window.getContext("2d")
-    },
-    player_num(num) {
-      this.pl_num = num
-    },
-    update_players(players) {
-      // Figure out time difference.
-      let cur_time = (new Date()).getTime();
-      this.recv_delta = cur_time - this.last_time;
-      this.delta_inc = 1000 / this.FPS / this.recv_delta;
-      this.frame_delta = 0;
-      this.last_time = cur_time;
+	 connect() {
+		console.log("Connected to server")
+		this.$socket.emit('enter_room')
+	 },
+	 game_over(players) {
+		this.win_list = players;
+		this.delta_inc = 0;
+		this.frame_delta = 0;
+	 },
+	 game_starts_in(data) {
+		this.start_draw_players = false
+		this.canv_players = []
+		this.count_down = data.count_down;
+		this.scale_window(data.settings)
+		this.init_players(data.players)
+		this.start_draw_players = true
+	 },
+	 init_settings(settings) {
+		// consistent naming
+		this.scale_window(settings)
+		this.ctx = this.$refs.game_window.getContext("2d")
+	 },
+	 player_num(num) {
+		this.pl_num = num
+	 },
+	 update_players(players) {
+		// Figure out time difference.
+		let cur_time = (new Date()).getTime();
+		this.recv_delta = cur_time - this.last_time;
+		this.delta_inc = 1000 / this.FPS / this.recv_delta;
+		this.frame_delta = 0;
+		this.last_time = cur_time;
 
-      // Add the new point to the path.
-      players.forEach(pl => {
-	let c_pl = this.canv_players[pl["num"]];
+		// Add the new point to the path.
+		players.forEach(pl => {
+		  let c_pl = this.canv_players[pl["num"]];
 
-	// Don't need to draw a line if not alive.
-	if (c_pl.alive) {
-	  c_pl.cur_x = c_pl.next_x;
-	  c_pl.cur_y = c_pl.next_y;
-	  c_pl["path"].lineTo(c_pl.next_x, c_pl.next_y);
-	  c_pl.next_x = this.grid_to_win(pl.x);
-	  c_pl.next_y = this.grid_to_win(pl.y);
-	}
+		  // Don't need to draw a line if not alive.
+		  if (c_pl.alive) {
+			 c_pl.cur_x = c_pl.next_x;
+			 c_pl.cur_y = c_pl.next_y;
+			 c_pl["path"].lineTo(c_pl.next_x, c_pl.next_y);
+			 c_pl.next_x = this.grid_to_win(pl.x);
+			 c_pl.next_y = this.grid_to_win(pl.y);
+		  }
 
-	c_pl.alive = pl["alive"]
-      });
-    },
-    searching_for_players() {
-      this.searching = true;
-    },
-    restart_search() {
-      this.count_down = 0
-      this.searching = true;
-      this.canv_players = []
-    },
-    start_game(players) {
-      this.count_down = 0;
-      this.searching = false;
-      this.init_players(players)
-    }
+		  c_pl.alive = pl["alive"]
+		});
+	 },
+	 searching_for_players() {
+		this.searching = true;
+	 },
+	 restart_search() {
+		this.count_down = 0
+		this.searching = true;
+		this.canv_players = []
+	 },
+	 start_game(players) {
+		this.count_down = 0;
+		this.searching = false;
+		this.init_players(players)
+	 }
   },
   methods: {
-    init_players(players) {
-      players.forEach(pl => {
-	let c_pl = {};
-	c_pl.color = pl["color"];
-	c_pl.next_x = this.grid_to_win(pl.x);
-	c_pl.next_y = this.grid_to_win(pl.y);
-	c_pl.cur_x = c_pl.next_x;
-	c_pl.cur_y = c_pl.next_y;
-	c_pl.num = pl.num;
-	c_pl.alive = pl.alive;
+	 init_players(players) {
+		players.forEach(pl => {
+		  let c_pl = {};
+		  c_pl.color = pl["color"];
+		  c_pl.next_x = this.grid_to_win(pl.x);
+		  c_pl.next_y = this.grid_to_win(pl.y);
+		  c_pl.cur_x = c_pl.next_x;
+		  c_pl.cur_y = c_pl.next_y;
+		  c_pl.num = pl.num;
+		  c_pl.alive = pl.alive;
 
-	let path = new Path2D();
-	path.moveTo(c_pl.next_x, c_pl.next_y);
-	c_pl.path = path;
+		  let path = new Path2D();
+		  path.moveTo(c_pl.next_x, c_pl.next_y);
+		  c_pl.path = path;
 
-	this.canv_players[pl["num"]] = c_pl;
-      });
-    },
-    grid_to_win(c) {
-      return c * this.grid-this.grid/2;
-    },
-    draw() {
-      if (this.ctx) {
-	this.draw_reset();
-	this.draw_grid();
-	if (this.start_draw_players) {
-	  this.draw_players();
-	  this.draw_lose();
-	} if (this.count_down > 0) {
-	  this.draw_count_down();
-	  this.draw_text_on_head("You");
-	} else if (this.win_list != null) {
-	  this.draw_win();
-	} else if (this.searching) {
-	  this.draw_text("Locating Socks...");
-	}
-      }
-    },
-    // Draws text centered.
-    draw_text(text, options = {}) {
-      let color = options.color || "white";
+		  this.canv_players[pl["num"]] = c_pl;
+		});
+	 },
+	 grid_to_win(c) {
+		return c * this.grid-this.grid/2;
+	 },
+	 draw() {
+		if (this.ctx) {
+		  this.draw_reset();
+		  this.draw_grid();
+		  if (this.start_draw_players) {
+			 this.draw_players();
+		  } if (this.count_down > 0) {
+			 this.draw_count_down();
+			 this.draw_text_on_head("you");
+		  } else if (this.win_list != null) {
+			 this.draw_game_over();
+		  } else if (this.searching) {
+			 this.draw_text("Locating Socks...");
+		  }
+		}
+	 },
+	 // Draws text centered.
+	 draw_text(text, options = {}) {
+		let color = options.color || "white";
 
-      let size = options.size || 60
-      this.ctx.save();
-      this.ctx.font = size + 'px \'Share Tech\'';
-      this.ctx.fillStyle = color;
+		let size = options.size || 60
+		this.ctx.save();
 
-      let tm = this.ctx.measureText(text);
-      let x_displacement = tm.width/2
-      let y_displacement = size * 1.2
-      let tx = (options.x || this.win_w/2) - x_displacement;
-      let ty = options.y || this.win_h/2;
+		// Cool TECH font
+		this.ctx.font = size + 'px \'Share Tech\'';
 
-      if (tx <= 0) tx += x_displacement
-      if (tx >= this.win_h) tx -= x_displacement
-      if (ty <= 0) {
-	ty += y_displacement
-	this.ctx.strokeStyle = '#111';
-	this.ctx.lineWidth = 2;
-	this.ctx.strokeText(text, tx, ty);
-      } else {
-	this.ctx.shadowOffsetX = 0;
-	this.ctx.shadowOffsetY = 0;
-	this.ctx.shadowBlur = 10;
-	this.ctx.shadowColor = '#111';
-      }
+		// For Text Fill
+		this.ctx.fillStyle = color;
 
-      this.ctx.fillText(text, tx, ty);
+		// For Text Outline (Looks like part of the shadow.
+		this.ctx.strokeStyle = '#000';
+		this.ctx.lineWidth = 2;
 
-      this.ctx.restore();
-    },
-    draw_count_down() {
-      this.draw_text(this.count_down.toString());
-    },
-    draw_lose() {
-      this.canv_players.forEach(pl => {
-	if (!pl.alive && this.win_list &&
-	    !this.win_list.find(w => w.num == pl.num) && pl.num == this.pl_num) {
-	  this.draw_text_on_head("You lose")
-	}
-      })
-    },
-    draw_win() {
-      // First, get the length.
-      let is_one_winner = this.win_list.length == 1
-      let color = is_one_winner ? this.win_list[0].color : "white";
-      let end_text = " Wins!";
+		// Align to the middle vertically.
+		this.ctx.textBaseline = 'middle';
 
-      if (!is_one_winner || this.win_list.find(w => w.num == this.pl_num)) {
-	end_text = " Win!";
-      }
+		// Shadow!
+		this.ctx.shadowOffsetX = 0;
+		this.ctx.shadowOffsetY = 0;
+		this.ctx.shadowBlur = 10;
+		this.ctx.shadowColor = '#000';
 
-      let name_list = this.win_list.map((pl) => pl.num == this.pl_num ? "You" : pl.num)
-      let text = name_list.join(" & ") + end_text;
-      this.draw_text(text, { color });
-    },
-    draw_text_on_head(message, pl_num = this.pl_num) {
-      let pl = this.canv_players[pl_num];
-      let x = pl.cur_x;
-      let y = pl.cur_y - this.pl_dim * 1.5;
-      let size = 25;
-      let color = pl.color
+		let tm = this.ctx.measureText(text);
+		let tx = (options.x || this.win_w/2) - tm.width/2;
+		let ty = options.y || this.win_h/2;
 
-      this.draw_text(message, { x, y, size, color });
-    },
-    draw_grid() {
-      this.ctx.save();
+		if (tx <= 0) tx = 0
+		if (tx >= this.win_w-tm.width) tx = this.win_w-tm.width;
 
-      this.ctx.strokeStyle = "rgba(100, 100, 100, .2)";
-      //this.ctx.setLineDash([5, 2.5]);
-      //this.ctx.lineDashOffset = 5;
-      this.ctx.lineWidth = 2;
+		if (ty <= size/2) ty = size/2;
+		if (ty >= this.win_h-size/2) ty = this.win_h-size/2;
 
-      this.ctx.beginPath();
-      for (let i = 0; i < this.grid_dim; i++) {
-	let x = i * this.grid;
-	this.ctx.moveTo(x, 0);
-	this.ctx.lineTo(x, this.win_h);
-      }
-      this.ctx.closePath();
-      this.ctx.stroke();
+		this.ctx.strokeText(text, tx, ty);
+		this.ctx.fillText(text, tx, ty);
 
-      this.ctx.beginPath();
-      for (let i = 0; i < this.grid_dim; i++) {
-	let y = i * this.grid;
-	this.ctx.moveTo(0,          y);
-	this.ctx.lineTo(this.win_w, y);
-      }
-      this.ctx.closePath();
-      this.ctx.stroke();
+		this.ctx.restore();
+	 },
+	 draw_count_down() {
+		this.draw_text(this.count_down.toString());
+	 },
+	 draw_game_over() {
+		this.canv_players.forEach(pl => {
+		  if (pl.alive) {
+			 this.draw_text_on_head("winner", pl.num);
+		  } else {
+			 this.draw_text_on_head("loser", pl.num);
+		  }
+		});
 
-      this.ctx.restore();
-    },
-    draw_reset() {
-      this.ctx.save();
-      this.ctx.clearRect(0, 0, this.win_w, this.win_h);
-      this.ctx.restore();
-    },
-    draw_players() {
-      this.ctx.save();
+		this.draw_text("Game Over");
+	 },
+	 draw_text_on_head(message, pl_num=this.pl_num) {
+		let pl = this.canv_players[pl_num];
+		let size = 25;
+		let color = pl.color
+		let x = pl.cur_x;
+		let y = pl.cur_y-this.grid;
 
-      // shadow
-      this.ctx.shadowOffsetX = 0;
-      this.ctx.shadowOffsetY = 0;
-      this.ctx.shadowBlur = 3;
-      this.ctx.shadowColor = '#000';
+		this.draw_text(message, { x, y, size, color });
+	 },
+	 draw_grid() {
+		this.ctx.save();
 
-      // line style
-      this.ctx.lineCap = 'round';
-      this.ctx.lineJoin = 'miter';
-      this.ctx.lineWidth = this.pl_dim; // was:   this.player.width;
+		this.ctx.strokeStyle = "rgba(100, 100, 100, .2)";
+		//this.ctx.setLineDash([5, 2.5]);
+		//this.ctx.lineDashOffset = 5;
+		this.ctx.lineWidth = 2;
 
-      // draw it!
-      this.canv_players.forEach((pl) => {
-	this.ctx.strokeStyle = pl["color"];
-	let path = pl["path"]
+		this.ctx.beginPath();
+		for (let i = 0; i < this.grid_dim; i++) {
+		  let x = i * this.grid;
+		  this.ctx.moveTo(x, 0);
+		  this.ctx.lineTo(x, this.win_h);
+		}
+		this.ctx.closePath();
+		this.ctx.stroke();
 
-	// If we are playing the game, then guess the next position.
-	if (pl.alive && this.win_list == null) {
-	  path = new Path2D(path);
-	  let x = (pl.next_x - pl.cur_x)*this.frame_delta;
-	  let y = (pl.next_y - pl.cur_y)*this.frame_delta;
+		this.ctx.beginPath();
+		for (let i = 0; i < this.grid_dim; i++) {
+		  let y = i * this.grid;
+		  this.ctx.moveTo(0,          y);
+		  this.ctx.lineTo(this.win_w, y);
+		}
+		this.ctx.closePath();
+		this.ctx.stroke();
 
-	  path.lineTo(pl.cur_x + x, pl.cur_y + y)
-	}
+		this.ctx.restore();
+	 },
+	 draw_reset() {
+		this.ctx.save();
+		this.ctx.clearRect(0, 0, this.win_w, this.win_h);
+		this.ctx.restore();
+	 },
+	 draw_players() {
+		this.ctx.save();
 
-	this.ctx.stroke(path);
-      });
+		// shadow
+		this.ctx.shadowOffsetX = 0;
+		this.ctx.shadowOffsetY = 0;
+		this.ctx.shadowBlur = 3;
+		this.ctx.shadowColor = '#000';
 
-      this.frame_delta += this.delta_inc;
-      if (this.frame_delta > 1) {
-	this.frame_delta = 1
-      }
-      // console.log(this.frame_delta, this.delta_inc, this.recv_delta)
+		// line style
+		this.ctx.lineCap = 'round';
+		this.ctx.lineJoin = 'miter';
+		this.ctx.lineWidth = this.pl_dim;
 
-      this.ctx.restore();
-    },
-    scale_window(settings) {
-      this.grid = settings.grid;
+		// draw it!
+		this.canv_players.forEach((pl) => {
+		  this.ctx.strokeStyle = pl["color"];
+		  let path = pl["path"]
 
-      // scale the grid
-      let smaller_dim = Math.min(window.innerHeight, window.innerWidth)
-      let win_size_in_percent = 0.95
-      let dim = Math.ceil(smaller_dim * win_size_in_percent / this.grid)
-      this.grid = dim / settings.grid_dim * this.grid
+		  // If we are playing the game, then guess the next position.
+		  if (pl.alive && this.win_list == null) {
+			 path = new Path2D(path);
+			 let x = (pl.next_x - pl.cur_x)*this.frame_delta;
+			 let y = (pl.next_y - pl.cur_y)*this.frame_delta;
 
-      this.grid_dim = settings.grid_dim
+			 path.lineTo(pl.cur_x + x, pl.cur_y + y)
+		  }
 
-      // scale the player
-      this.pl_dim = this.grid * 0.8
+		  this.ctx.stroke(path);
+		});
 
-      this.win_w = this.grid_dim * this.grid;
-      this.win_h = this.grid_dim * this.grid;
-    },
-    key_handler(e) {
-      let key = keys[e.which]
-      if (key) this.$socket.emit('keydown', key)
-    }
+		this.frame_delta += this.delta_inc;
+		if (this.frame_delta > 1) {
+		  this.frame_delta = 1
+		}
+
+		this.ctx.restore();
+	 },
+	 scale_window(settings) {
+		this.grid = settings.grid;
+
+		// scale the grid
+		let smaller_dim = Math.min(window.innerHeight, window.innerWidth)
+		let win_size_in_percent = 0.95
+		let dim = Math.ceil(smaller_dim * win_size_in_percent / this.grid)
+		this.grid = dim / settings.grid_dim * this.grid
+
+		this.grid_dim = settings.grid_dim
+
+		// scale the player
+		this.pl_dim = this.grid * 0.8
+
+		this.win_w = this.grid_dim * this.grid;
+		this.win_h = this.grid_dim * this.grid;
+	 },
+	 key_handler(e) {
+		let key = keys[e.which]
+		if (key) this.$socket.emit('keydown', key)
+	 }
   },
   mounted() {
-    // Vue's keydown event only really works for input elements
-    // so we have to bind the event the old fashioned way
-    window.addEventListener('keydown', this.key_handler)
+	 // Vue's keydown event only really works for input elements
+	 // so we have to bind the event the old fashioned way
+	 window.addEventListener('keydown', this.key_handler)
   },
 }
 </script>
