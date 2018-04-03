caa98f8e8eb771952e2e022f6ad533bc4748b9a6
quademorrison <quademorrison@gmail.com>
2018-04-02 23:26:20 -0600

    Floor blocks_per_player

--- server/settingtools.py
+++ server/settingtools.py
@@ -4,7 +4,7 @@ import math
 # Calculate how many blocks for the grid dimensions
 # so we can scale dynamically
 def grid_dimensions(num_players):
-    blocks_per_player = settings.area_constant / math.sqrt(num_players)
+    blocks_per_player = math.floor(settings.area_constant / math.sqrt(num_players))
     return max(blocks_per_player * num_players, settings.min_dimension)
 
 # Settings to be delivered to the client in a form
